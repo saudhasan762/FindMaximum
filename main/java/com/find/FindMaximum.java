9b2d19245ad0aa13a09385fbdd5305cf8647d6c2
saudhasan762 <azanali152@gmail.com>
2021-03-02 00:46:40 +0530

    Refactored

--- main/java/com/find/FindMaximum.java
+++ main/java/com/find/FindMaximum.java
@@ -5,11 +5,6 @@ import java.util.Collections;
 import java.util.List;
 
 public class FindMaximum<E> {
-    private E[] myArray;
-
-    public FindMaximum(E[] myArray) {
-        this.myArray = myArray;
-    }
 
     public FindMaximum() {
     }
